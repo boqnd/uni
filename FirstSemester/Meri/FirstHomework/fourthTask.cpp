53a01af2c2c80a52bf84e290321a67b36d46d466
boyandonchev <boyandon@gmail.com>
2020-12-02 13:32:48 +0200

    fourth task meri

---
+++ FirstSemester/Meri/FirstHomework/fourthTask.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int roses, tulips, chrysants;
+  char season, holiday;
+
+  cin >> chrysants >> roses >> tulips >> season >> holiday;
+
+  const double chrysantsPrice[2] = {2.00, 3.75}, rosesPrice[2] = {4.10, 4.50}, tulipsPrice[2] = {2.50, 4.15};
+
+  
+
+
+  return 0;
+}
