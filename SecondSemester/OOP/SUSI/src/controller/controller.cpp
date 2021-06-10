2c0440e2dbad31f54875093ad6861f6c7e0c967b
boyandonchev <boyandon@gmail.com>
2021-06-10 03:02:47 +0300

    change command added and fixed open command bug

--- SecondSemester/OOP/SUSI/src/controller/controller.cpp
+++ SecondSemester/OOP/SUSI/src/controller/controller.cpp
@@ -35,179 +35,183 @@ public:
     if (this->fileName != nullptr) {
       std::ifstream in(this->fileName);
 
-      while (!in.eof()){
-        String type;
-        in >> type;
+      if (in.good()) {
+        while (!in.eof()){
+          String type;
+          in >> type;
 
-        if (type == "Discipline") {
-          Discipline d;
-          String _name, _required, _year;
-          in >> _name >> _required >> _year;
+          if (type == "Discipline") {
+            Discipline d;
+            String _name, _required, _year;
+            in >> _name >> _required >> _year;
 
-          d.setName(_name);
-          if (_required == "0") {
-            d.setRequired(false);
-          } else if (_required == "1") {
-            d.setRequired(true);
-          } else {
-            throw std::invalid_argument( "received non-bool value from file" );
-          }
-
-          d.setYear(_year.parseInt());
-
-          bool disciplineFound = false;
-          for (size_t i = 0; i < this->disciplines.getSize(); i++)
-          {
-            if (_name == this->disciplines[i]->getName()) {
-              disciplineFound = true;
-
-              std::cout << std::endl << "WARNING: duplicating discipline names found in the file. Only the first will be saved." << std::endl << std::endl;
-
-              break;
+            d.setName(_name);
+            if (_required == "0") {
+              d.setRequired(false);
+            } else if (_required == "1") {
+              d.setRequired(true);
+            } else {
+              throw std::invalid_argument( "received non-bool value from file" );
             }
-          }
 
-          if (!disciplineFound) {
-            this->disciplines.push_back(new Discipline(d));
-          }
-        } else if (type == "Program") {
-          Program p;
-          String _name, _count;
-          in >> _name >> _count;
-          
-          p.setName(_name);
+            d.setYear(_year.parseInt());
 
-          for (size_t i = 0; i < _count.parseInt(); i++)
-          {
-            String currentDiscipline;
-            in >> currentDiscipline;
+            bool disciplineFound = false;
+            for (size_t i = 0; i < this->disciplines.getSize(); i++)
+            {
+              if (_name == this->disciplines[i]->getName()) {
+                disciplineFound = true;
+
+                std::cout << std::endl << "WARNING: duplicating discipline names found in the file. Only the first will be saved." << std::endl << std::endl;
+
+                break;
+              }
+            }
+
+            if (!disciplineFound) {
+              this->disciplines.push_back(new Discipline(d));
+            }
+          } else if (type == "Program") {
+            Program p;
+            String _name, _count;
+            in >> _name >> _count;
+            
+            p.setName(_name);
+
+            for (size_t i = 0; i < _count.parseInt(); i++)
+            {
+              String currentDiscipline;
+              in >> currentDiscipline;
+
+              bool disciplineFound = false;
+              for (size_t d = 0; d < this->disciplines.getSize(); d++)
+              {
+                if(currentDiscipline == this->disciplines[d]->getName()) {
+                  bool duplicateDiscipline = false;
+                  for (size_t pd = 0; pd < p.getDisciplines().getSize(); pd++)
+                  {
+                    if (currentDiscipline == p.getDisciplines()[pd]->getName()) {
+                      duplicateDiscipline = true;
+                      std::cout << std::endl << "WARNING: duplicating disciplines in the same program found in the file. Only one will be saved." << std::endl << std::endl;
+
+                      break;
+                    }
+                    
+                  }
+                  
+                  if (!duplicateDiscipline) {
+                    p.addDiscipline(this->disciplines[d]);
+                  }
+
+                  disciplineFound = true;
+                }
+              }
+
+              if (!disciplineFound) {
+                throw std::invalid_argument( "received non-existing discipline name from file" );
+              }
+            }
+
+            bool programFound = false;
+            for (size_t i = 0; i < this->programs.getSize(); i++)
+            {
+              if (_name == this->programs[i]->getName()) {
+                programFound = true;
+
+                std::cout << std::endl << "WARNING: duplicating program names found in the file. Only the first will be saved." << std::endl << std::endl;
+
+                break;
+              }
+            }
+
+            if (!programFound) {
+              this->programs.push_back(new Program(p));
+            }
+          } else if (type == "Student") {
+            String _name, _fn, _program, _group, _year, _status, _averageGrade;
+            Student s;
+        
+            in >> _name >> _fn >> _program >> _group >> _year >> _status >> _averageGrade;
+
+            s.setName(_name);
+            s.setFn(_fn.parseInt());
+            s.setGroup(_group.parseInt());
+            s.setYear(_year.parseInt());
+            s.setStatus((Status)_status.parseInt());
+            s.setAverageGrade(_averageGrade.parseDouble());
+
+            bool programFound = false;
+            for (size_t p = 0; p < this->programs.getSize(); p++)
+            {
+              if(_program == this->programs[p]->getName()) {
+                programFound = true;
+                s.setProgram(this->programs[p]);
+                break;
+              }
+            }
+
+            if (!programFound) {
+              throw std::invalid_argument( "received non-existing program name from file" );
+            }
+
+            bool studentFound = false;
+            for (size_t i = 0; i < this->students.getSize(); i++)
+            {
+              if(_name == this->students[i]->getName()) {
+                studentFound = true;
+
+                std::cout << std::endl << "WARNING: duplicating student names found in the file. Only the first will be saved." << std::endl << std::endl;
+
+                break;
+              }
+            }
+
+            if (!studentFound) {
+              this->students.push_back(new Student(s));
+            }
+          } else if (type == "Record") {
+
+            String _studentFn, _discipline, _grade;
+            Record r;
+        
+            in >> _studentFn >> _discipline >> _grade;
+
+            bool studentFound = false;
+            for (size_t s = 0; s < this->students.getSize(); s++)
+            {
+              if(_studentFn.parseInt() == this->students[s]->getFn()) {
+                studentFound = true;
+                r.setStudent(this->students[s]);
+                break;
+              }
+            }
+
+            if (!studentFound) {
+              throw std::invalid_argument( "received non-existing student fn from file" );
+            }
 
             bool disciplineFound = false;
             for (size_t d = 0; d < this->disciplines.getSize(); d++)
             {
-              if(currentDiscipline == this->disciplines[d]->getName()) {
-                bool duplicateDiscipline = false;
-                for (size_t pd = 0; pd < p.getDisciplines().getSize(); pd++)
-                {
-                  if (currentDiscipline == p.getDisciplines()[pd]->getName()) {
-                    duplicateDiscipline = true;
-                    std::cout << std::endl << "WARNING: duplicating disciplines in the same program found in the file. Only one will be saved." << std::endl << std::endl;
-
-                    break;
-                  }
-                  
-                }
-                
-                if (!duplicateDiscipline) {
-                  p.addDiscipline(this->disciplines[d]);
-                }
-
+              if(_discipline == this->disciplines[d]->getName()) {
                 disciplineFound = true;
+                r.setDiscipline(this->disciplines[d]);
+                break;
               }
             }
 
             if (!disciplineFound) {
               throw std::invalid_argument( "received non-existing discipline name from file" );
             }
+
+            r.setGrade(_grade.parseDouble());
+
+            records.push_back(new Record(r));
           }
-
-          bool programFound = false;
-          for (size_t i = 0; i < this->programs.getSize(); i++)
-          {
-            if (_name == this->programs[i]->getName()) {
-              programFound = true;
-
-              std::cout << std::endl << "WARNING: duplicating program names found in the file. Only the first will be saved." << std::endl << std::endl;
-
-              break;
-            }
-          }
-
-          if (!programFound) {
-            this->programs.push_back(new Program(p));
-          }
-        } else if (type == "Student") {
-          String _name, _fn, _program, _group, _year, _status, _averageGrade;
-          Student s;
-      
-          in >> _name >> _fn >> _program >> _group >> _year >> _status >> _averageGrade;
-
-          s.setName(_name);
-          s.setFn(_fn.parseInt());
-          s.setGroup(_group.parseInt());
-          s.setYear(_year.parseInt());
-          s.setStatus((Status)_status.parseInt());
-          s.setAverageGrade(_averageGrade.parseDouble());
-
-          bool programFound = false;
-          for (size_t p = 0; p < this->programs.getSize(); p++)
-          {
-            if(_program == this->programs[p]->getName()) {
-              programFound = true;
-              s.setProgram(this->programs[p]);
-              break;
-            }
-          }
-
-          if (!programFound) {
-            throw std::invalid_argument( "received non-existing program name from file" );
-          }
-
-          bool studentFound = false;
-          for (size_t i = 0; i < this->students.getSize(); i++)
-          {
-            if(_name == this->students[i]->getName()) {
-              studentFound = true;
-
-              std::cout << std::endl << "WARNING: duplicating student names found in the file. Only the first will be saved." << std::endl << std::endl;
-
-              break;
-            }
-          }
-
-          if (!studentFound) {
-            this->students.push_back(new Student(s));
-          }
-        } else if (type == "Record") {
-
-          String _studentFn, _discipline, _grade;
-          Record r;
-      
-          in >> _studentFn >> _discipline >> _grade;
-
-          bool studentFound = false;
-          for (size_t s = 0; s < this->students.getSize(); s++)
-          {
-            if(_studentFn.parseInt() == this->students[s]->getFn()) {
-              studentFound = true;
-              r.setStudent(this->students[s]);
-              break;
-            }
-          }
-
-          if (!studentFound) {
-            throw std::invalid_argument( "received non-existing student fn from file" );
-          }
-
-          bool disciplineFound = false;
-          for (size_t d = 0; d < this->disciplines.getSize(); d++)
-          {
-            if(_discipline == this->disciplines[d]->getName()) {
-              disciplineFound = true;
-              r.setDiscipline(this->disciplines[d]);
-              break;
-            }
-          }
-
-          if (!disciplineFound) {
-            throw std::invalid_argument( "received non-existing discipline name from file" );
-          }
-
-          r.setGrade(_grade.parseDouble());
-
-          records.push_back(new Record(r));
         }
-      }
+      } else {
+        std::cout << "Successfully created " << this->fileName << std::endl;
+      } 
       
       in.close();
 
@@ -420,16 +424,137 @@ public:
         std::cout << "exception: " << e.what() << std::endl << std::endl;
       } 
     } else if (command == "change") {
-      //todo
-                  //       if (students[studentIndex]->getStatus() == Interupted)
-                  // {
-                  //   std::cout << "Student " << students[studentIndex]->getName() << " is interrupted. Cant't enrol." << std::endl;
-                  // } else if (students[studentIndex]->getStatus() == Gradueted)
-                  // {
-                  //   std::cout << "Student " << students[studentIndex]->getName() << " is graduated. Cant't enrol." << std::endl;
-                  // } else if (students[studentIndex]->getStatus() == Enrolled)
-                  // {
-      std::cout << "----- not implemented -----" << std::endl;
+      try{
+        if (this->fileName != nullptr) {
+          if ((words.getSize() > 3)) {
+            unsigned int _fn = words[1].parseInt();
+            String option = words[2];
+            String value = words[3];
+
+            int studentIndex = -1;
+
+            for (size_t i = 0; i < students.getSize(); i++)
+            {
+              if (_fn == students[i]->getFn())
+              {
+                studentIndex = i;
+                break;
+              }
+            }
+
+            if (studentIndex > -1)
+            {
+              if (students[studentIndex]->getStatus() == Interupted)
+              {
+                std::cout << "Student " << students[studentIndex]->getName() << " is interrupted. Cant't change." << std::endl;
+              } else if (students[studentIndex]->getStatus() == Gradueted)
+              {
+                std::cout << "Student " << students[studentIndex]->getName() << " is graduated. Cant't change." << std::endl;
+              } else if (students[studentIndex]->getStatus() == Enrolled) {
+                if (option == "program") {
+                  String _program = value;
+
+                  int programIndex = -1;
+
+                  for (size_t j = 0; j < programs.getSize(); j++)
+                  {
+                    if (_program == programs[j]->getName())
+                    {
+                      programIndex = j;
+                      break;
+                    }
+                  }
+
+                  if (programIndex > -1)
+                  {
+                    bool allTaken = true;
+
+                    for (size_t i = 0; i < programs[programIndex]->getDisciplines().getSize(); i++)
+                    {
+                      for (size_t r = 0; r < records.getSize(); r++)
+                      {
+                        if ((programs[programIndex]->getDisciplines()[i]->getName() == records[r]->getDiscipline()->getName()) && (students[studentIndex]->getFn() == records[r]->getStudent()->getFn()) && (programs[programIndex]->getDisciplines()[i]->getYear() <= students[studentIndex]->getYear()) && (programs[programIndex]->getDisciplines()[i]->isRequired())) {
+                          if (records[r]->getGrade() < 3)
+                          {
+                            allTaken = false;
+                            break;
+                          }
+                        }
+                      }
+                    }
+                    if (allTaken)
+                    {
+                      students[studentIndex]->setProgram(programs[programIndex]);
+                      std::cout << "Successfully changed the program of student " << students[studentIndex]->getName() << std::endl;
+                    }else {
+                      std::cout << "Not all required exams are passed. Can't change program." << std::endl;
+                      
+                    }
+                  } else {
+                    std::cout << "No program with name " << _program << " was found" << std::endl;
+                  }
+                  // boli boli ... bol, kalo alchohol
+                } else if (option == "group") {
+                  int _group = value.parseInt();
+                  
+                  students[studentIndex]->setGroup(_group);
+
+                  std::cout << "Successfully changed the group of student " << students[studentIndex]->getName() << std::endl;
+                } else if (option == "year") {
+                  int _year = value.parseInt();
+                  
+                  if (students[studentIndex]->getYear() + 1 == _year)
+                  {
+                    int notTakenCount = 0;
+                    for (size_t i = 0; i < students[studentIndex]->getProgram()->getDisciplines().getSize(); i++)
+                    {
+                      bool currTaken = false;
+                      for (size_t r = 0; r < records.getSize(); r++)
+                      {
+                        if ((students[studentIndex]->getProgram()->getDisciplines()[i]->getName() == records[r]->getDiscipline()->getName()) && (students[studentIndex]->getFn() == records[r]->getStudent()->getFn() ) && (students[studentIndex]->getProgram()->getDisciplines()[i]->isRequired()) ) {
+                          if (records[r]->getGrade() >= 3)
+                          {
+                            currTaken = true;
+                            break;
+                          }
+                        }
+                      }
+                      if (!currTaken)
+                      {
+                        notTakenCount++;
+                      }
+                    }
+                    
+                    if(notTakenCount <= 2) {
+                      students[studentIndex]->advance();
+                      std::cout << "Successfully advanced student " << students[studentIndex]->getName() << std::endl;
+                    } else {
+                      std::cout << students[studentIndex]->getName() << " has more then 2 not taken required exams. Can't advance" << std::endl;
+                    }
+
+                  } else {
+                    std::cout << "Cant't change students year to other then the next year."<< std::endl;
+                  }
+                  
+
+                } else {
+                  std::cout << option << " is not a valid option." << std::endl;
+                }
+              }
+            } else {
+              std::cout << "Student with fn " << _fn << " does not exist." << std::endl;
+            }
+          } else {
+            std::cout << "Failed to change." << std::endl;
+          } 
+        } else {
+          std::cout << "No opened file." << std::endl;
+        }
+      } catch(const std::exception& e) {
+        std::cout << std::endl << "|    <error>    |" << std::endl << std::endl;
+
+        std::cout << "exception: " << e.what() << std::endl << std::endl;
+      } 
     } else if (command == "graduate") {
       try{
         if (this->fileName != nullptr) {
