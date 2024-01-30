f6bfb6aae632d82129216f0c46ce10977ba083b2
Boyan Donchev <boyandon@gmail.com>
2024-01-30 15:58:50 +0200

    fix db uri

--- SeventhSemester/sebiz/cannonball/api/db/Client.js
+++ SeventhSemester/sebiz/cannonball/api/db/Client.js
@@ -2,6 +2,7 @@ import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
 
 const uri = "mongodb+srv://cannonball:cannonball@cannonball.gcvkcdq.mongodb.net/?retryWrites=true&w=majority";
 
+// const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1";
 export class Client {
   constructor() {
     this.client = new MongoClient(uri, {
